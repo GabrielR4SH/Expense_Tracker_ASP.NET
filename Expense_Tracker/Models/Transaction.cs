cb9689cc5fba8fa290c7bbb3bbb0c674ceb70923
GabrielR4SH <bieelsantana23@hotmail.com>
2024-04-01 21:43:03 -0300

    Adicionar arquivos de projeto.

---
+++ Expense_Tracker/Models/Transaction.cs
@@ -0,0 +1,15 @@
+﻿namespace Expense_Tracker.Models
+{
+    public class Transaction
+    {
+        public int TransactionID { get; set; }
+
+        //CategoryID
+
+        public int Amount { get; set; }
+
+        public string? Note { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
